2f91b9430ea355fe96ae2b09b893da721001063b
B-Dock <Midknight@users.noreply.github.com>
2026-04-25 23:34:29 -0600

    Initial Core Shield site
    Uploading the initial Core Shield site from my local build.

---
+++ .eleventy.js
@@ -0,0 +1,53 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, options = {}) {
+  const {
+    sizes,
+    loading = "lazy",
+    class: className = "",
+    widths = [400, 800, 1200, 1600],
+    formats = ["webp", "jpeg"],
+    fetchpriority,
+    decoding = "async"
+  } = options;
+
+  const metadata = await Image(src, {
+    widths,
+    formats,
+    outputDir: "./_site/assets/images/",
+    urlPath: "/assets/images/",
+    filenameFormat: function (id, src, width, format) {
+      const pathParts = src.split("/");
+      const originalName = pathParts.pop().split(".")[0];
+      return `${originalName}-${width}.${format}`;
+    }
+  });
+
+  const imageAttributes = {
+    alt,
+    loading,
+    decoding
+  };
+
+  if (sizes) imageAttributes.sizes = sizes;
+  if (className) imageAttributes.class = className;
+  if (fetchpriority) imageAttributes.fetchpriority = fetchpriority;
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/css");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts");
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    }
+  };
+};
