4ac1022f072627e33fd12957024e690ef9d85413
B-Dock <Midknight@users.noreply.github.com>
2026-06-08 22:36:00 -0600

    Added a downloadable brochure
    Added the asset and links to allow visitors to download the PDF brochure.

--- .eleventy.js
+++ .eleventy.js
@@ -40,6 +40,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/css");
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
   eleventyConfig.addPassthroughCopy("src/assets/favicon");
+  eleventyConfig.addPassthroughCopy("src/assets/downloads");
 
   eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
